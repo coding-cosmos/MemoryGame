75ee41410586fe2e0eda32c544ba93f0f42e011c
Sujal <115341188+coding-cosmos@users.noreply.github.com>
2024-04-23 19:09:20 +0530

    Add basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
 
 function App() {
- 
-
   return (
-    <>
-      
-    </>
+    <div className="container">
+      <div className="nav">
+        <nav>
+          <div className="logo"><img src="../public/memory.png" alt="" />
+            <span className="logo-text">MemGame</span>
+          </div>
+          <div className="counter-container">counters</div>
+        </nav>
+      </div>
+      <div className="main">
+        <section className="game">
+         
+        </section>
+      </div>
+    </div>
   )
 }
 
