a08df2e5126b3187838e6c7a331a35d64b6ed3bc
Sujal <115341188+coding-cosmos@users.noreply.github.com>
2024-04-24 15:40:37 +0530

    Add handleClick function
    --Randomize the order
    --Update the card state

--- src/components/CardsHolder.jsx
+++ src/components/CardsHolder.jsx
@@ -1,12 +1,45 @@
 import "../styles/CardsHolder.css";
 import { Card } from "./Card";
 import { randomInRange } from "../scripts/Utils";
+import { useState } from "react";
 
 export function CardsHolder() {
+  const [ clicked, setClicked ] = useState(false);
+  const [ cardsState, setCardsState ] = useState([]);
+
+  function handleClick(event, index) {
+    // Randomize the cards due to state change
+    clicked ? setClicked(false) : setClicked(true);
+
+    // change state of card
+    if (cardsState[ index ] == true) {
+      setCardsState([]);
+      // update current score counter
+      // set high score counter
+    } else {
+       let temp = cardsState;
+       temp[index] = true;
+       setCardsState(temp);
+       console.log(temp)
+      
+      // update current score counter
+      // update high score counter
+    }
+     
+  }
+
   return (
     <div className="cardsholder">
       {randomInRange(15).map((index) => {
-        return <Card index={index} key={index} />;
+        return (
+          <Card
+            index={index}
+            key={index}
+            onClick={(e) => {
+              handleClick(e, index);
+            }}
+          />
+        );
       })}
     </div>
   );
