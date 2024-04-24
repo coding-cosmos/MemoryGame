57e66a8ebd87420489e3a080529ec5b5355b1f3d
Sujal <115341188+coding-cosmos@users.noreply.github.com>
2024-04-24 14:54:17 +0530

    Add utility function to get random order

---
+++ src/scripts/Utils.js
@@ -0,0 +1,11 @@
+export function randomInRange(n) {
+  const range = [];
+  while (range.length != n) {
+    let index = Math.floor(Math.random() * (n + 1));
+    if (!range.includes(index) && index != 0) {
+      range.push(index);
+    }
+  }
+  return range;
+}
+
